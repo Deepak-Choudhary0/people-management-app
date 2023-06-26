986bb268696c2e0f91325c89f1fbefda40ea68b6
Deepak Choudhary <deepakchoudharysl78@gmail.com>
2023-06-26 09:19:02 +0530

    Updated

--- src/app/backend/index.js
+++ src/app/backend/index.js
@@ -21,6 +21,8 @@ const connectToMongoDB = async () => {
       
       const collection = db.collection('person_data');
     
+    // CRUD on MongoDB
+
     // // Create data
 
     //   // Single document insertion
@@ -36,39 +38,71 @@ const connectToMongoDB = async () => {
     //   const results = await collection.insertMany(documents);
     //   console.log('Inserted document IDs:', results.insertedIds);
 
-    //Read
-      const documents = await collection.find().toArray();
-      // console.log('All documents:', documents);
+    //Reading Part
+      var documents = await collection.find().toArray();
+      console.log('\nAll documents:',documents.length,'\n');
 
+      //Get keys for index 0, as other keys gonna be same too for other indixes
       const keys = Object.keys(documents[0]);
-      // console.log(keys);
+      // Iterate through the object properties
+      for (let i=0;i<documents.length;i++){
+        console.log(`Data ID: ${i}`);
+        for (x in keys){
+          key=keys[x];
+          console.log(`==> ${key} : ${documents[i][key]}`);
+        }
+      };
 
-    // Iterate through the object properties
-    console.log(documents.length);
+    // Update
+      const documentToUpdate = { _id: documents[0][keys[0]] };
+      const updateFields = { $set: { age: 40 } };
+      const updateResult = await collection.updateOne(documentToUpdate, updateFields);
+      console.log('\nUpdated document count:', updateResult.modifiedCount,'\n');
 
-    // // Update
-    //   const documentToUpdate = { _id: new ObjectId('<documentId>') }; // Replace <documentId> with the actual document ID
-    //   const updateFields = { $set: { age: 35 } };
-    //   const updateResult = await collection.updateOne(documentToUpdate, updateFields);
-    //   console.log('Updated document count:', updateResult.modifiedCount);
+      console.log('Updated Data:');
 
-    // // Delete
-    //   const documentToDelete = { _id: new ObjectId('<documentId>') }; // Replace <documentId> with the actual document ID
-    //   const deleteResult = await collection.deleteOne(documentToDelete);
-    //   console.log('Deleted document count:', deleteResult.deletedCount);
+      //Get data again from DB
+      documents = await collection.find().toArray();
+      for (x in keys){
+        key=keys[x];
+        console.log(`==> ${key} : ${documents[0][key]}`);
+      }
 
-    // //Query
-    //   const query = { age: { $gte: 30 } }; // Example query to find documents with age greater than or equal to 30
-    //   const documents = await collection.find(query).toArray();
+    // Delete
+      const documentToDelete = { _id: documents[0][keys[0]] }; // Replace <documentId> with the actual document ID
+      const deleteResult = await collection.deleteOne(documentToDelete);
+      console.log('\nDeleted document count:', deleteResult.deletedCount,'\n');
 
-    //   if (documents.length > 0) {
-    //     console.log('Matching documents:');
-    //     documents.forEach((document, index) => {
-    //       console.log(`Document ${index + 1}:`, document);
-    //     });
-    //   } else {
-    //     console.log('No matching documents found.');
-    //   }
+      console.log('Data after deletion:');
+
+      //Get data again from DB
+      documents = await collection.find().toArray();
+      for (let i=0;i<documents.length;i++){
+        console.log(`Data ID: ${i}`);
+        for (x in keys){
+          key=keys[x];
+          console.log(`==> ${key} : ${documents[i][key]}`);
+        }
+      };
+
+    //Query
+      const query = { age: { $gte: 35 } }; // Example query to find documents with age greater than or equal to 30
+      const documentsMatching = await collection.find(query).toArray();
+      if (documentsMatching.length > 0) {
+        console.log('\nMatching documents:',documentsMatching.length,'\n');
+        // documentsMatching.forEach((document, index) => {
+        //   console.log(`Document ${index + 1}:`, document);
+        // });
+        for (let i=0;i<documentsMatching.length;i++){
+          console.log(`Matching ID: ${i}`);
+          for (x in keys){
+            key=keys[x];
+            console.log(`==> ${key} : ${documentsMatching[i][key]}`);
+          }
+        };
+      } else {
+        console.log('No matching documents found.');
+      }
     } 
     else {
       console.log(`The collection '${collectionName}' does not exist in the 'person' database.`);
